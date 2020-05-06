63e507d794c76ecc4d0a40ecbd1d68c9644968bd
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-06 18:43:11 +0200

    Añadiendo un componente para mi header y le doy estilos con clases de materialize

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = ({titulo}) => {
+    return ( 
+        <nav>
+            <div className="nav-wrapper ligh-blue darken-2">
+                
+            </div>
+        </nav>
+
+     );
+}
+ 
+export default Header;
