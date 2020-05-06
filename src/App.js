e15fa8795bfb8ebb99b9d6aa280e17d978c2803c
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-06 21:49:30 +0200

    Declarando las variables para la llamada a la API

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
+import React, { Fragment, useState, useEffect } from 'react'
+import Header from './components/Header'
+import Formulario from './components/Formulario'
 
-function App() {
+function App () {
+  // State del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
 
-// State del formulario
-const [ busqueda, guardarBusqueda ] = useState({
-  ciudad: '',
-  pais: ''
-});
+  const [consultar, guardarConsultar] = useState(false)
 
-const [ consultar, guardarConsultar ] = useState(false);
+  const { ciudad, pais } = busqueda
 
-const { ciudad, pais } = busqueda;
-
-useEffect(() => {
-  const consultarAPI = async () => {
-    
-  }
-  consultarAPI();
-},[consultar]);
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const appId = '93e94d836748716962d0b76919eb50e4'
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=guadalajara,mexico&
+      appid=93e94d836748716962d0b76919eb50e4`
+    }
+    consultarAPI()
+  }, [consultar])
 
   return (
     <Fragment>
-      <Header 
-        titulo='Clima React App'
-      />
+      <Header titulo='Clima React App' />
 
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
-            <div className="col m6 s12">
-               <Formulario 
+      <div className='contenedor-form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col m6 s12'>
+              <Formulario
                 busqueda={busqueda}
                 guardarBusqueda={guardarBusqueda}
                 guardarConsultar={guardarConsultar}
-               />
+              />
             </div>
-            <div className="col m6 s12">
-                2
-            </div>
+            <div className='col m6 s12'>2</div>
           </div>
         </div>
       </div>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
