c80c12174b1a30b8b3b70c03407aecc3d09c644a
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-07 22:25:31 +0200

    Recalculando los grados restando los grados kelvin

--- src/components/Clima.js
+++ src/components/Clima.js
@@ -7,13 +7,16 @@ const Clima = ({resultado}) => {
     const { name, main } = resultado;
 
     if (!name) return null;     
+
+    // Grados Kelvin
+    const kelvin =273.15;
     
     return ( 
         <div className="card-panel white col s12">
             <div className="black-text">
                 <h2>El clima de {name} es:</h2>
                 <p className="temperatura">
-                    {main.temp}
+                    {main.temp-kelvin}
                 </p>
             </div>
         </div>
